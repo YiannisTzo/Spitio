618ccacd89a13abe9e05f70f5d3eb35cd19fd672
Ioannis-Tzomiadis-NTT <ioannis.tzomiadis@nttdata.com>
2026-09-17 16:00:23 +0100

    Spitio phase 3 in progress

--- infrastructure/terraform/environments/dev/main.tf
+++ infrastructure/terraform/environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "../../modules/resource-group"
+  source   = "../../modules/resource-group"
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
